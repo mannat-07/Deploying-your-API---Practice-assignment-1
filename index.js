2d937afed2e9b872fc2fa014a61ea49bcba0c555
Mannat Garg <mannatgarg2006@gmail.com>
2025-02-17 22:24:41 +0530

    ENV

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
+require('dotenv').config();
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+console.log(config);
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/user', (req, res) => {
+  res.send(config);
 });
 
 app.listen(port, () => {
